f4e6a9a504e339d2ec2d331d097084dbdc5b941f
sunnydaydev <1246771571@qq.com>
2019-09-16 18:14:15 +0800

    multi item通用的adapter实现。

--- app/src/main/java/com/sunnyday/materialdesigndemo/adapter/MultiItemBaseAdapter.java
+++ app/src/main/java/com/sunnyday/materialdesigndemo/adapter/MultiItemBaseAdapter.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 public abstract class MultiItemBaseAdapter<T> extends BaseAdapter<T> {
 
+
     protected MultiItemTypeSupport<T> mMultiItemTypeSupport;
 
     public MultiItemBaseAdapter(Context context, List<T> data, MultiItemTypeSupport<T> multiItemTypeSupport) {
